91f5febb61be119ecaa20c98318c65822aba34a4
Y Sai Sampath <ysampath36@gmail.com>
2024-11-14 10:23:40 +0530

    Updated Package

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sampath.github.io",
+  base: "/portfolio-old",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
